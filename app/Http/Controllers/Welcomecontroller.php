231e20b1e923a65e43e912b9ced5ef09de2e36b8
Volkan Haci <Volkan Haci>
2016-01-23 23:13:34 +0100

    Initial commit

---
+++ app/Http/Controllers/Welcomecontroller.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class Welcomecontroller extends Controller
+{
+    public function index() {
+        
+        return 'Welcome to Laravel Lab';
+        
+    }
+    
+    
+    
+    
+    
+    
+}
